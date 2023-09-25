b65569cd45faceceb1bb14a0c84a7b07cffdde12
Nadim Nahle <nadim.nahle@itxi.net>
2023-09-25 16:32:11 +0300

    convert virtual books to real books

--- src/components/BookList/Book.jsx
+++ src/components/BookList/Book.jsx
@@ -2,32 +2,32 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import "./BookList.css";
 
-const Book = (book) => {
+const Book = ({item}) => {
   return (
     <div className='book-item flex flex-column flex-sb'>
       <div className='book-item-img'>
-        <img src = {book.cover_img} alt = "cover" />
+        <img src = {item.image} alt = "cover" />
       </div>
       <div className='book-item-info text-center'>
-        <Link to = {`/book/${book.id}`} {...book}>
+        <Link to = {`/book/`} >
           <div className='book-item-info-item title fw-7 fs-18'>
-            <span>{book.title}</span>
+            <span>{item.title}</span>
           </div>
         </Link>
 
         <div className='book-item-info-item author fs-15'>
           <span className='text-capitalize fw-7'>Author: </span>
-          <span>{book.author.join(", ")}</span>
+          <span>{item.author}</span>
         </div>
 
         <div className='book-item-info-item edition-count fs-15'>
-          <span className='text-capitalize fw-7'>Total Editions: </span>
-          <span>{book.edition_count}</span>
+          <span className='text-capitalize fw-7'>Genre: </span>
+          <span>{item.type}</span>
         </div>
 
         <div className='book-item-info-item publish-year fs-15'>
           <span className='text-capitalize fw-7'>First Publish Year: </span>
-          <span>{book.first_publish_year}</span>
+          <span>{item.year}</span>
         </div>
       </div>
     </div>
