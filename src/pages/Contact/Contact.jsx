7f79420f1e67564231713b392abb8337fa50b502
Nadim Nahle <nadim.nahle@itxi.net>
2023-09-28 14:24:29 +0300

    update contact

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Contact.css";
 import Navbar from "../../components/Navbar/Navbar"
+import { Link } from "react-router-dom";
 
 // npm i @emailjs/browser
 
@@ -10,7 +11,13 @@ const ContactForm = () => {
         <>
             <Navbar ></Navbar>
             <section class="contact-us" id="contact-section">
-                <form id="contact" action="" method="post">
+                <div className="left">
+                    <div className="info"><div className="title">Name: </div>Dr. Soha Yaghi (She/her)</div>
+                    <div className="info"><div className="title">linkdin: </div>   <a target="_blank" href="http://www.linkedin.com/in/soha-yaghi-phd-98828722b">soha-yaghi-phd</a></div>
+                    <div className="info"><div className="title">Address1: </div>Liban, Beirut, Achrafieh, Sassine Square</div>
+                    <div className="info"><div className="title">Address2: </div>France, Paris, 9 Rue des Colonnes, 75002</div>
+                </div>
+                <div className="right"><form id="contact" action="" method="post">
 
                     <div class="section-heading">
                         <h4>Contact us</h4>
@@ -35,7 +42,7 @@ const ContactForm = () => {
                         <button type="submit" id="form-submit" class="main-gradient-button" disabled>Send a message</button>
                     </div>
 
-                </form>
+                </form></div>
             </section>
         </>
     )
