eaf2df0b3af6566a13ce44418bbeb0466cf314a2
Nadim Nahle <nadim.nahle@itxi.net>
2023-09-28 14:23:25 +0300

    updatebook details

--- src/components/BookDetails/BookDetails.jsx
+++ src/components/BookDetails/BookDetails.jsx
@@ -1,35 +1,34 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import Loading from "../Loader/Loader";
 import coverImg from "../../images/cover_not_found.jpg";
 import "./BookDetails.css";
-import {FaArrowLeft} from "react-icons/fa";
+import { FaArrowLeft } from "react-icons/fa";
 import { useNavigate } from 'react-router-dom';
-
-const URL = "https://openlibrary.org/works/";
+import books from "../../books.json";
+import Navbar from '../Navbar/Navbar';
 
 const BookDetails = () => {
-  const {id} = useParams();
+  const { id } = useParams();
   const [loading, setLoading] = useState(false);
   const [book, setBook] = useState(null);
   const navigate = useNavigate();
 
   useEffect(() => {
     setLoading(true);
-    async function getBookDetails(){
-      try{
-        const response = await fetch(`${URL}${id}.json`);
-        const data = await response.json();
+    async function getBookDetails() {
+      try {
+        const data = books[id - 1];
         console.log(data);
-
-        if(data){
-          const {description, title, covers, subject_places, subject_times, subjects} = data;
+        if (data) {
+          const { details: description, title, image: covers, subject_places, subject_times, subjects } = data;
+          console.log(covers, 'a')
           const newBook = {
-            description: description ? description.value : "No description found",
+            description: description ? description : "No description found",
             title: title,
-            cover_img: covers ? `https://covers.openlibrary.org/b/id/${covers[0]}-L.jpg` : coverImg,
+            cover_img: covers ? covers : coverImg,
             subject_places: subject_places ? subject_places.join(", ") : "No subject places found",
-            subject_times : subject_times ? subject_times.join(", ") : "No subject times found",
+            subject_times: subject_times ? subject_times.join(", ") : "No subject times found",
             subjects: subjects ? subjects.join(", ") : "No subjects found"
           };
           setBook(newBook);
@@ -37,7 +36,7 @@ const BookDetails = () => {
           setBook(null);
         }
         setLoading(false);
-      } catch(error){
+      } catch (error) {
         console.log(error);
         setLoading(false);
       }
@@ -45,43 +44,47 @@ const BookDetails = () => {
     getBookDetails();
   }, [id]);
 
-  if(loading) return <Loading />;
+  if (loading) return <Loading />;
 
   return (
-    <section className='book-details'>
-      <div className='container'>
-        <button type='button' className='flex flex-c back-btn' onClick={() => navigate("/book")}>
-          <FaArrowLeft size = {22} />
-          <span className='fs-18 fw-6'>Go Back</span>
-        </button>
+    <>
+      <Navbar />
 
-        <div className='book-details-content grid'>
-          <div className='book-details-img'>
-            <img src = {book?.cover_img} alt = "cover img" />
-          </div>
-          <div className='book-details-info'>
-            <div className='book-details-item title'>
-              <span className='fw-6 fs-24'>{book?.title}</span>
+      <section className='book-details'>
+        <div className='container'>
+          <button type='button' className='flex flex-c back-btn' onClick={() => navigate("/book")}>
+            <FaArrowLeft size={22} />
+            <span className='fs-18 fw-6'>Go Back</span>
+          </button>
+
+          <div className='book-details-content grid'>
+            <div className='book-details-img'>
+              <img src={book?.cover_img} alt="cover img" />
             </div>
-            <div className='book-details-item description'>
-              <span>{book?.description}</span>
-            </div>
-            <div className='book-details-item'>
-              <span className='fw-6'>Subject Places: </span>
-              <span className='text-italic'>{book?.subject_places}</span>
-            </div>
-            <div className='book-details-item'>
-              <span className='fw-6'>Subject Times: </span>
-              <span className='text-italic'>{book?.subject_times}</span>
-            </div>
-            <div className='book-details-item'>
-              <span className='fw-6'>Subjects: </span>
-              <span>{book?.subjects}</span>
+            <div className='book-details-info'>
+              <div className='book-details-item title'>
+                <span className='fw-6 fs-24'>{book?.title}</span>
+              </div>
+              <div className='book-details-item description'>
+                <span>{book?.description}</span>
+              </div>
+              <div className='book-details-item'>
+                <span className='fw-6'>Subject Places: </span>
+                <span className='text-italic'>{book?.subject_places}</span>
+              </div>
+              <div className='book-details-item'>
+                <span className='fw-6'>Subject Times: </span>
+                <span className='text-italic'>{book?.subject_times}</span>
+              </div>
+              <div className='book-details-item'>
+                <span className='fw-6'>Subjects: </span>
+                <span>{book?.subjects}</span>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   )
 }
 
