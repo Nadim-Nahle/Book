b77d6c3e806df4c77befc8bb31a2e429ef0f6516
Nadim Nahle <nadim.nahle@itxi.net>
2023-10-16 10:13:35 +0300

    create artistList

---
+++ src/components/ArtistList/ArtistList.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useGlobalContext } from '../../context.';
+import artists from "../../artist.json";
+import Navbar from '../Navbar/Navbar';
+import Artist from './Artist';
+
+//https://covers.openlibrary.org/b/id/240727-S.jpg
+
+const ArtistList = (book) => {
+  const { resultTitle } = useGlobalContext();
+  return (
+    <>
+      <Navbar />
+      <section className='booklist'>
+        <div className='container'>
+          <div className='section-title'>
+            <h2>{resultTitle}</h2>
+          </div>
+          <div className='booklist-content grid'>
+            {
+              artists.map((item) => {
+                return (
+                  <Artist item={item} key={item.id} />
+                )
+              })
+            }
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default ArtistList
