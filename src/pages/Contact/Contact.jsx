1d0c3cab8b3a3a407741d4d4b013ef6a9abc8a05
Nadim Nahle <nadim.nahle@itxi.net>
2023-10-16 11:09:17 +0300

    Update contact form

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,46 +1,59 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./Contact.css";
 import Navbar from "../../components/Navbar/Navbar"
+import emailjs from '@emailjs/browser';
 
 // npm i @emailjs/browser
 
 const ContactForm = () => {
 
+    const form = useRef();
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_iv4tcob', 'template_08o8yfi', form.current, 'tL6sKd8DVGyIeSbAS')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+    };
+
     return (
         <>
             <Navbar ></Navbar>
-            <section class="contact-us" id="contact-section">
+            <section className="contact-us" id="contact-section">
                 <div className="left">
                     <div className="info"><div className="title">Name: </div>Dr. Soha Yaghi (She/her)</div>
-                    <div className="info"><div className="title">linkdin: </div>   <a target="_blank" rel="noreferrer" href="http://www.linkedin.com/in/soha-yaghi-phd-98828722b">soha-yaghi-phd</a></div>
+                    <div className="info"><div className="title">linkdin: </div>   <a target="_blank" rel="noreferrer" href="http://www.linkedin.com/in/soha-yaghi-phd">soha-yaghi-phd</a></div>
+                    <div className="info"><div className="title">Email: </div>s.yaghi@sohaliteraryartsagency.com</div>
                     <div className="info"><div className="title">Address1: </div>Liban, Beirut, Achrafieh, Sassine Square</div>
                     <div className="info"><div className="title">Address2: </div>France, Paris, 9 Rue des Colonnes, 75002</div>
                 </div>
-                <div className="right"><form id="contact" action="" method="post">
+                <div className="right">
+                <form id="contact" ref={form} onSubmit={sendEmail}>
 
-                    <div class="section-heading">
+                    <div className="section-heading">
                         <h4>Contact us</h4>
                     </div>
-
-                    <div class="inputField">
-                        <input type="name" name="name" id="name" placeholder="Your name" autocomplete="on" required />
-                        <span class="valid_info_name"></span>
+                    <div className="inputField">
+                        <input type="name" name="from_name" id="name" placeholder="Your name" autoComplete="on" required />
+                        <span className="valid_info_name"></span>
                     </div>
 
-                    <div class="inputField">
-                        <input type="Email" name="email" id="email" placeholder="Your email" required="" />
-                        <span class="valid_info_email"></span>
+                    <div className="inputField">
+                        <input type="Email" name="from_email" id="email" placeholder="Your email" required="" />
+                        <span className="valid_info_email"></span>
                     </div>
 
-                    <div class="inputField">
+                    <div className="inputField">
                         <textarea name="message" id="message" placeholder="Your message"></textarea>
-                        <span class="valid_info_message"></span>
+                        <span className="valid_info_message"></span>
                     </div>
 
-                    <div class="inputField btn">
-                        <button type="submit" id="form-submit" class="main-gradient-button" disabled>Send a message</button>
+                    <div className="inputField btn">
+                        <button type="submit" id="form-submit" className="main-gradient-button">Send a message</button>
                     </div>
-
                 </form></div>
             </section>
         </>
