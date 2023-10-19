7d9a9a14b0ae4a35093cf302266107d44078c914
Nadim Nahle <nadim.nahle@itxi.net>
2023-10-19 10:44:00 +0300

    Update author css

--- src/components/AuthorDetails/AuthorDetails.jsx
+++ src/components/AuthorDetails/AuthorDetails.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import Loading from "../Loader/Loader";
 import coverImg from "../../images/cover_not_found.jpg";
 import "./AuthorDetails.css";
@@ -52,7 +52,7 @@ const AuthorDetails = () => {
 
       <section className='book-details'>
         <div className='container'>
-          <button type='button' className='flex flex-c back-btn' onClick={() => navigate("/book")}>
+          <button type='button' className='flex flex-c back-btn' onClick={() => navigate("/artist")}>
             <FaArrowLeft size={22} />
             <span className='fs-18 fw-6'>Go Back</span>
           </button>
@@ -75,7 +75,10 @@ const AuthorDetails = () => {
                 </span>
               </div>
             </div>
-          </div>
+            <Link to={`/gallery`}>
+            <div className='art-btn'>Click here to check his art collection</div>
+            </Link>
+          </div>       
         </div>
       </section>
     </>
